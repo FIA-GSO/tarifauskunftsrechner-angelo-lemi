32ed923cfa0eef9d86afaa6c3795e7778132bac1
Angelo <72870190+angelocharne@users.noreply.github.com>
2024-10-08 11:28:30 +0200

    Update main.py
    Jugendliche Preis, erneute abfrage hinzugefügt, erste Abfrage in Function gepackt

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 preis_erwachsene = 5.0
 preis_kinder = 2.5
+preis_jungendlich = 3.5
 preis_premium = 3.0
 preis_basis = 4.0
-test = 0
 
-print(" ### Tarifauskunftsrechner Museum XXX ### ")
-print(" Hallo, geben Sie bitte Ihr Alter ein.")
-alter_gast = int(input())
+def Tarif():
+    print(" ### Tarifauskunftsrechner Museum XXX ### ")
+    print(" Hallo, geben Sie bitte Ihr Alter ein.")
+    alter_gast = int(input())
 
-if alter_gast < 14:
-    print(" ### Eintritt Kinder ### ")
-    print(" Preis: ", preis_kinder, " Euro ")
-else:
-    print(" Sind Sie Mitglied im Duisburger Museumsclub? (Nachweis erforderlich) ")
-    print(" Wenn Sie Premium-Mitglied sind, geben Sie 'p' ein.")
-    print(" Wenn Sie Basis-Mitglied sind, geben Sie 'b' ein.")
-    print(" Wenn Sie kein Mitglied sind, drücken Sie eine beliebige andere Taste. ")
-    antwort_rabatt = input();
-    if antwort_rabatt == "p":
-        print(" ### Eintritt Premium-Mitglied ### ")
-        print(" Preis: ", preis_premium, " Euro ")
-        if antwort_rabatt == "b":
-            print(" ### Eintritt Basis-Mitglied ### ")
-            print(" Preis: ", preis_basis, " Euro ")
-        else:
-            print(" ### Eintritt Erwachsene (voller Preis) ### ")
-            print(" Preis: ", preis_erwachsene, " Euro")
+    if alter_gast < 14:
+        print(" ### Eintritt Kinder ### ")
+        print(" Preis: ", preis_kinder, " Euro ")
+    elif 14 <= alter_gast < 18:
+        print(" ### Eintritt Jugendliche ### ")
+        print(" Preis: ", preis_jungendlich, " Euro ")
+    else:
+        print(" Sind Sie Mitglied im Duisburger Museumsclub? (Nachweis erforderlich) ")
+        print(" Wenn Sie Premium-Mitglied sind, geben Sie 'p' ein.")
+        print(" Wenn Sie Basis-Mitglied sind, geben Sie 'b' ein.")
+        print(" Wenn Sie kein Mitglied sind, drücken Sie eine beliebige andere Taste. ")
+        antwort_rabatt = input()
+        if antwort_rabatt == "p":
+            print(" ### Eintritt Premium-Mitglied ### ")
+            print(" Preis: ", preis_premium, " Euro ")
+            if antwort_rabatt == "b":
+                print(" ### Eintritt Basis-Mitglied ### ")
+                print(" Preis: ", preis_basis, " Euro ")
+            else:
+                print(" ### Eintritt Erwachsene (voller Preis) ### ")
+                print(" Preis: ", preis_erwachsene, " Euro")
 
+Tarif()
+
+print("Wollen sie einen weiteren Tarif abfragen?")
+tarif_abfrage = input()
+if tarif_abfrage == "y":
+    Tarif()
+elif tarif_abfrage == "n":
+    print("Viel Spaß!")
